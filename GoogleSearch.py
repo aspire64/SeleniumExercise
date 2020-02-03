8047d526a5555b2c2630fef51acfe595206e5f01
shsrinivasan <shobhita687@gmail.com>
2020-02-03 14:06:39 -0500

    Initial Code Commit

--- GoogleSearch.py
+++ GoogleSearch.py
@@ -4,12 +4,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 # Assign the location of the Chrome Driver installed on your local machine
-driver = webdriver.Chrome(executable_path="/Users/shsrinivasan/Downloads/chromedriver")
+driver = webdriver.Chrome(executable_path="/Users/shsrinivasan/Downloads/chromedriver") #-- ChromeDriver
+#driver = webdriver.Chrome(executable_path="/Users/shsrinivasan/Downloads/geckodriver") # -- GeckoDriver for Firefox
+
 
 driver.maximize_window()
 
 # Navigate to www.google.com
 driver.get("https://www.google.com")
+
 
 # Locate the Search Box and enter the "Search String" and start search
 searchButton = driver.find_element_by_xpath("//*[@name='q']")
